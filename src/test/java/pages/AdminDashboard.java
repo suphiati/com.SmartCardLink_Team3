688b8bee483ed3affe4fbb22aa5fc57dd298a1e3
Eren Gok <mehmeterengok66@gmail.com>
2023-10-27 18:02:17 +0300

    Eren/27.10.2023/adminDashboard guncellendi

--- src/test/java/pages/AdminDashboard.java
+++ src/test/java/pages/AdminDashboard.java
@@ -32,4 +32,62 @@ public class AdminDashboard {
     //AdminDashboard delete oldu bildirim yazisi
     @FindBy(xpath = "//*[@class='swal-button swal-button--confirm']")
     public WebElement usersSayfasiDeleteOnayYazisi;
+
+    //AdminDashboard FrontCMS sekmesi
+    @FindBy(xpath = "//span[text()='Front CMS']")
+    public WebElement frontCmsSayfasi;
+
+    //AdminDashboard FrontCMS sekmesi ==> Testimonials butonu
+    @FindBy(xpath = "(//a[@class='nav-link p-0 '])[21]")
+    public WebElement frontCmsSayfasiTestimonialsButton;
+
+    @FindBy(xpath = "(//*[@class='d-flex align-items-center'])[2]")
+    public WebElement gorusGoruntulemeElementi;
+
+    //AdminDashboard FrontCMS sekmesi ==> Testimonials butonu==>gorus icerik goruntuleme elementi
+    @FindBy(xpath = "(//*[@class='btn px-1 text-info fs-3 view-testimonial-btn '])[1]")
+    public WebElement gorusIcerikGoruntulemeElementi;
+
+    //AdminDashboard FrontCMS sekmesi ==> Testimonials butonu==> gorus icerik goruntuleme elementi ==> gorus icerik goruntulenebildigi kontrol elementi
+    @FindBy(xpath = "//*[text()='New Testimonial']")
+    public WebElement gorusIcerikGoruntulemeKontrolElementi;
+
+    //AdminDashboard FrontCMS sekmesi ==> Testimonials butonu==>gorus duzenleme butonu
+    @FindBy(xpath = "(//*[@class='btn px-1 text-primary fs-3 front-testimonial-edit-btn'])[1]")
+    public WebElement gorusDuzenlemeElementi;
+
+    //AdminDashboard FrontCMS sekmesi ==> Testimonials butonu==>gorus duzenleme butonu==>DescriptionKutusu
+    @FindBy(xpath = "//*[@id='editDescription']")
+    public WebElement gorusDuzenlemeElementiDescriptionKutusu;
+
+    //AdminDashboard FrontCMS sekmesi ==> Testimonials butonu==>gorus duzenleme butonu==>Save kutusu
+    @FindBy(xpath = "//*[@id='testimonialUpdate']")
+    public WebElement gorusDuzenlemeElementiSaveKutusu;
+
+    //AdminDashboard FrontCMS sekmesi ==> Testimonials butonu==>gorus duzenleme butonu==>Save kutusu==>Success yazisi
+    @FindBy(xpath = "//div[@class'=toast toast-success']")
+    public WebElement gorusDuzenlemeSuccessYazisi;
+
+
+
+    //AdminDashboard FrontCMS sekmesi ==> Testimonials bolumu Add Testimonials butonu
+    @FindBy(xpath = "//*[@class='btn btn-primary ms-auto']")
+    public WebElement addTestimonialButton;
+
+    //AdminDashboard FrontCMS sekmesi ==> Testimonials bolumu Add Testimonials sonrasi ==>Name kutusu
+    @FindBy(xpath = "//*[@id='name']")
+    public WebElement addTestimonialNameBox;
+
+    //AdminDashboard FrontCMS sekmesi ==> Testimonials bolumu Add Testimonials sonrasi ==>Description kutusu
+    @FindBy(xpath = "//*[@id='description']")
+    public WebElement addTestimonialDescriptionBox;
+
+    //AdminDashboard FrontCMS sekmesi ==> Testimonials bolumu Add Testimonials sonrasi ==>Resim ekleme butonu
+    @FindBy(xpath = "(//*[@id='profileImageIcon'])[1]")
+    public WebElement addTestimonialAddImage;
+
+    //AdminDashboard FrontCMS sekmesi ==> Enquiries butonu
+    @FindBy(xpath = "(//a[@class='nav-link p-0 '])[22]")
+    public WebElement frontCmsSayfasiEnquiriesButton;
+
 }
