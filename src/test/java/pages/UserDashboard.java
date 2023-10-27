b91d23fbd71cb6da607328bce6e4238ab53be89b
Resul Acar <acar-resul-38@hotmail.com>
2023-10-27 18:16:54 +0300

    27/10/2023 resul/v3

--- src/test/java/pages/UserDashboard.java
+++ src/test/java/pages/UserDashboard.java
@@ -1,4 +1,44 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class UserDashboard {
+    public UserDashboard(){
+
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    // Dashboard yazisi elemnti
+    @FindBy(xpath = "//a[@class='nav-link p-0 active']")
+    public WebElement dashboardYazisi;
+
+    //  Total Active Vcards yazisi elementi
+    @FindBy(linkText = "Total Active VCards")
+    public WebElement totalActiveVCardsYazisi;
+
+    //  Total DeActive VCards yazisi elementi
+    @FindBy(linkText = "Total DeActive VCards")
+    public WebElement totalDeActiveVCardsYazisi;
+    //  Today Enquiries yazisi elementi
+    @FindBy(linkText = "Today Enquiries")
+    public WebElement todayEnquiriesYazisi;
+    //   Today Appointments yazisi elementi
+    @FindBy(linkText = "Today Appointments")
+    public WebElement todayAppointmentsYazisi;
+    //    Vcard Analytic yazisi elementi
+    @FindBy(linkText = " Vcard Analytic")
+    public WebElement vcardAnalyticYazisi;
+    //Vcard Analytic grafik tablo
+    @FindBy(xpath = "//div[@id='dashboardWeeklyUserBarChartContainer']")
+    public WebElement vcardAnalyticGrafikSekli;
+    //Today Appointments Randevu bilgisi bölümü
+    @FindBy(linkText = "Today Appointments")
+    public WebElement todayAppointmentsRandevuBilgiBolumu;
+    //Switch Charts linki
+    @FindBy(xpath = "//button[@title='Switch Chart']")
+    public WebElement switchChartsButton;
+
 }
